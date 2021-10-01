877d9890ac4a1efad81c4e18465d623f217e2f97
julien3641 <3641@holbertonschool.com>
2021-10-01 16:33:25 +0200

    de z à a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Smile in the mirror de z à a
+ *
+ * Return: return (0)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	{
+		for (n = 'z'; n <= 'a'; ++n)
+			putchar(n);
+		putchar('\n');
+	}
+/* your code goes there */
+	return (0);
+}
