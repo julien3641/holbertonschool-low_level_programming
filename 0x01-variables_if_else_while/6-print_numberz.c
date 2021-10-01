c4563980f1a004d5fbabd17c893c0a647d1a7fa4
julien3641 <3641@holbertonschool.com>
2021-10-01 16:27:18 +0200

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Print numberz
+ *
+ * Return: return (0)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	{
+		for (n = '0'; n <= '9'; n++)
+			putchar(n);
+		putchar('\n');
+	}
+/* your code goes there */
+	return (0);
+}
