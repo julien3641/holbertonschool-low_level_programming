87d488131dffb46dac82c70be49ade323bb553f9
julien3641 <3641@holbertonschool.com>
2021-10-05 17:15:25 +0200

    Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - affiche l'alphabet x10
+ *
+ *
+ */
+int _islower(int c)
+{
+	if  (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
