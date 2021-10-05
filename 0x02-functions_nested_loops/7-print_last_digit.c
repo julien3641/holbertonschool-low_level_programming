9ccb90a1dbb2bd4592a047fef17c5f0a2c33b993
julien3641 <3641@holbertonschool.com>
2021-10-05 21:04:33 +0200

    maj 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,11 @@
  */
 int print_last_digit(int n)
 {
-	if (n > 0)
-	{
-		_putchar(n % 10 + '0');
-		return (n % 10);
-	}
-	else
-	{
-		n = n * -1;
-		_putchar(n % 10 + '0');
-		return (n % 10);
-	}
+	n = n % 10;
+
+	if (n < 0)
+		n = -1 * n;
+
+	_putchar('0' + n);
+	return (n);
 }
