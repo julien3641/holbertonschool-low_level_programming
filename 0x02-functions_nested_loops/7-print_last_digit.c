621703b94a4bc910bea7bc28ea8aedef1647d2c1
julien3641 <3641@holbertonschool.com>
2021-10-05 18:46:03 +0200

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - affiche l'alphabet x10
+ *
+ * @n: The character to print
+ *
+ * Return: 1 ou 0 ou 98
+ */
+int _abs(int n)
+{
+		return (n % 10);
+}
