11afc31b41a53396bff62e11382776850894a1c7
julien3641 <3641@holbertonschool.com>
2021-10-07 21:04:11 +0200

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - de 1 à 100 Fizz Fuzz FizzBuzz
+ *
+ * Return: 1 or 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 15 == 0))
+			printf("FizzBuzz");
+		else if	(i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
