6c50545f6c4a646784c4640c05cc8d7babef88ab
julien3641 <3641@holbertonschool.com>
2021-10-07 22:05:09 +0200

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a straiht line
+ * @size: valeur d'arrêt
+ *
+ * Return: 1 or 0
+ */
+
+void print_triangle(int size)
+{
+	int etage;
+	int espace;
+	int dieze;
+
+	if (size <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (etage = 0; etage < size; etage++)
+		{
+			for (espace = size - etage; espace > 1; espace--)
+			{
+				_putchar ('.');
+			}
+			{
+				for (dieze = 0; dieze <= etage; dieze++)
+				{
+					_putchar ('#');
+				}
+				_putchar ('\n');
+			}
+		}
+	}
+}
