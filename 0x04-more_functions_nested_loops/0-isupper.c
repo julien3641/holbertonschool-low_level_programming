c297f0efd026131696c618c553f695b3a359890d
julien3641 <3641@holbertonschool.com>
2021-10-07 09:58:06 +0200

    isupper check betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - est en majuscule
- *
+ * @c: int
  * Return: 1 or 0
  */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c  <= 90)
-		return(1);
+		return (1);
 	else
-			return(0);
+		return (0);
 }
