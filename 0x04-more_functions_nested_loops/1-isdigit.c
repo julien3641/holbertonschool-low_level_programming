3dd1372e71427f13a38146bd228bfb0415159e02
julien3641 <3641@holbertonschool.com>
2021-10-07 10:17:45 +0200

    idigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - est en majuscule
+ * @c: int
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c  <= 57)
+		return (1);
+	else
+		return (0);
+}
