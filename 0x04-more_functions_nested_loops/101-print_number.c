863dd923f3b10217fadeb825826c59c0e95ac71e
julien3641 <3641@holbertonschool.com>
2021-10-08 15:51:40 +0200

    print number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers
+ *
+ *
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar(45);
+		_putchar(n = (-n));
+	}
+	else
+		_putchar(n);
+}
