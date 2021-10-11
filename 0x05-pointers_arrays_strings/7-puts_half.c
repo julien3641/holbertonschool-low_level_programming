e3e20be57af2b36c8ea234b045afc9fefb23ddc1
julien3641 <3641@holbertonschool.com>
2021-10-11 15:10:56 +0200

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - longeur d'un champs
+ * @str: valeur de str
+ *
+ * Return: Return 0
+ */
+void puts_half(char *str)
+{
+	int n = strlen(str);
+	int i;
+	{
+		if (n % 2 == 0)
+			for (i = n / 2; i < n; i++)
+			{
+				_putchar(str[i]);
+			}
+		else
+			for (i = n / 2 + 1; i < n; i++)
+			{
+				_putchar(str[i]);
+			}
+		_putchar ('\n');
+	}
+}
