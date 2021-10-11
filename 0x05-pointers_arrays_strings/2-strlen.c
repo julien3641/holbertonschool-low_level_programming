ed7fb0615deee2ebc4bf415f82bd9fbad6998670
julien3641 <3641@holbertonschool.com>
2021-10-11 11:06:46 +0200

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - longeur d'un champs
+ * @s: valeur de a
+ *
+ * Return: Return 0
+ */
+int _strlen(char *s)
+{
+	int nbcar;
+	{
+		nbcar = strlen(s);
+			}
+	return (nbcar);
+}
