2eccd11669321978be5bc96e8ab6736fdd9e1687
julien3641 <3641@holbertonschool.com>
2021-10-11 11:21:44 +0200

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts - longeur d'un champs
+ * @str: valeur de a
+ *
+ * Return: Return 0
+ */
+void _puts(char *str)
+{
+	puts(str);
+	_putchar (0);
+}
