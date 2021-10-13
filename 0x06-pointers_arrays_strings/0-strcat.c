3e4b9e168ee4cbf81b5a58c4e1df75476107b47c
julien3641 <3641@holbertonschool.com>
2021-10-13 13:05:57 +0200

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatener
+ *
+ * @dest: destination
+ * @src: src
+ */
+char *_strcat(char *dest, char *src)
+{
+	return(strcat (dest, src));
+}
