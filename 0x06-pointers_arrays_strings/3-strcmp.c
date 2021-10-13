31bd2452e90d889251bd3f11c4472c73c9200d1d
julien3641 <3641@holbertonschool.com>
2021-10-13 13:41:03 +0200

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare Two Strings
+ *
+ * @s1: destination
+ * @s2: src
+ *
+ * Return: true or false
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
