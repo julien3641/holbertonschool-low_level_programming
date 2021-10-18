7af66ae2d0c3d5390f1eaef931b745c2e73f9f3f
julien3641 <3641@holbertonschool.com>
2021-10-18 13:34:16 +0200

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strstr - return a constent byte
+ *
+ * @haystack: char
+ * @needle: char
+ *
+ * Return: 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+
+{
+	return (strstr(haystack, needle));
+}
