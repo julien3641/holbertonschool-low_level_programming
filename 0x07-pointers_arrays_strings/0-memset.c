614c0bb60a11b5cbe20a181a931798e034111486
julien3641 <3641@holbertonschool.com>
2021-10-18 11:06:18 +0200

    memset function to increment bytes

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _memset - return a constent byte
+ *
+ * @s: char
+ * @b: char
+ * @n: unsigned int
+ * Return: 0
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	return (memset(s, b, n));
+}
