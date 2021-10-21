5541723a2b5a84253d0caf2202d0945b75d2e6b6
julien3641 <3641@holbertonschool.com>
2021-10-21 15:59:45 +0200

    find if a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * string_length - calculate the number of string
+ * @s: s
+ *
+ * Return: nb string
+ */
+int string_length(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + string_length (s + 1));
+}
+
+/**
+ * palindrome_check- check if the first letter is the same as the last one
+ * @s: s
+ *
+ * Return: return
+ */
+int palindrome_check(char *s, int len, int i)
+{
+	if (s[i] == s[len / 2])
+	{
+		return (1);
+	}
+	if (s[i] == s[len - i - 1])
+	{
+		return (palindrome_check(s, len, i + 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - test if *s is null
+ * @s: s
+ * Return: natural square root or -1
+*/
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int len = string_length(s);
+
+        if (*s == '\0')
+                return (1);
+        return (palindrome_check (s, len, i));
+}
