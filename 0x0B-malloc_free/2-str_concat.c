ad0a548c7bdf7dfe3ead4781a1816fa314a266b9
julien3641 <3641@holbertonschool.com>
2021-10-26 14:35:40 +0200

    Concatenate s1 and s2 in s3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * str_concat - concatenate
+ * @s1: s1
+ * @s2: s2
+ * Return: 0
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	char *s3;
+
+	if (s1 == 0 || s2 == 0)
+	{
+		return (0);
+	}
+	s3 = malloc(k * sizeof(char) + 1);
+	if (s3 == 0)
+	{
+		return (0);
+	}
+	while (s1[i] != '\0')
+	{
+		s3[k] = s1[i];
+		i++;
+		k++;
+	}
+	while (s2[j] != '\0')
+	{
+		s3[k] = s2[j];
+		j++;
+		k++;
+	}
+	s3[k] = '\0';
+	return (s3);
+}
