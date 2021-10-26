c49f4f972ac1dfa805aa48f08d8a9ff956cf5664
julien3641 <3641@holbertonschool.com>
2021-10-26 11:13:03 +0200

    create a array and allocate the size of the array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * create_array - create an array of chars
+ * @size: size
+ * @c: c
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int n;
+	char *ar;
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+		ar = malloc(size * sizeof(char));
+	for (n = 0; n < size; n++)
+		ar[n] = c;
+	return (ar);
+}
