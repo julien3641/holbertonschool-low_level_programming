63ee52303a9d262a42f7ad1d4432e31b51ab76ba
julien3641 <3641@holbertonschool.com>
2021-10-28 12:48:30 +0200

    less than 40 lines for betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,10 +11,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int k = 0;
-	unsigned int l = 0;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *s3;
 
 	if (s1 == 0)
