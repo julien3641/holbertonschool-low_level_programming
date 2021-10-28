fd1e944de2961f417d09ece12f4b3873dac2777d
julien3641 <3641@holbertonschool.com>
2021-10-28 11:48:01 +0200

    concatenate 2 strings with a limite for the second one

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenate
+ * @s1: s1
+ * @s2: s2
+ * @n: n
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int k = 0;
+	unsigned int l = 0;
+	char *s3;
+
+	if (s1 == 0)
+		i = 0;
+	else
+	{
+		while (s1[i])
+			i++;
+	}
+	if (s2 == 0)
+		j = 0;
+	else
+	{
+		while (s2[j])
+			j++;
+	}
+	if (n >= j)
+	l = i + j;
+	else
+		l = i + n;
+	s3 = malloc(sizeof(char) * (l + 1));
+	if (s3 == 0)
+		return (0);
+	j = 0;
+	while (k < l)
+	{
+		if (k < i)
+			s3[k] = s1[k];
+		else
+		{
+			s3[k] = s2[j];
+			j++;
+		}
+		k++;
+	}
+	s3[k] = '\0';
+	return (s3);
+}
