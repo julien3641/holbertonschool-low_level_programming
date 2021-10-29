d04d5b72d3ab14ca635781d17e41d016e1024b22
julien3641 <3641@holbertonschool.com>
2021-10-29 10:47:45 +0200

    realloc memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 
-int _strlen(char *s);
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 
@@ -19,32 +18,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	void *nptr;
 
-	ptr = malloc(old_size);
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == 0)
+	{
 		ptr = malloc(new_size);
+		return (ptr);
+	}
 	if (new_size == 0 && ptr != NULL)
 		return (NULL); /* is equivalent to free(ptr)*/
 	nptr = malloc(new_size);
-	_memcpy(nptr, ptr, _strlen(ptr));
+	_memcpy(nptr, ptr, old_size);
 	free(ptr);
 	return (nptr);
-}
-
-/**
- * _strlen - count the number of caracter in recursion
-* @s: s
-* Return: 0
-*/
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
 }
 
 /**
