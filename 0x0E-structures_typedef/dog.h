1e3efc1772c00d2a1584834b5b9dbcf3678df16c
julien3641 <3641@holbertonschool.com>
2021-10-30 22:45:26 +0200

    stucture for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+/**
+ * struct dog - define the dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: Name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+#endif
