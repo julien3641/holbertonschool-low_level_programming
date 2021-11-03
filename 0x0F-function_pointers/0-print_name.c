e48e37ed0372ca91f512d4b6ef08ffb0e82a888c
julien3641 <3641@holbertonschool.com>
2021-11-03 12:49:47 +0100

    .h

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,10 @@
 #include "function_pointers.h"
-
 /**
  * print_name - print name
  * @f: call to a function
  * @name: name
  * Return: 0
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == 0 || f == 0)
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTIONS_POINTERS
 #define FUNCTIONS_POINTERS
+#include <stddef.h>
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-#include <stddef.h>
 #endif
