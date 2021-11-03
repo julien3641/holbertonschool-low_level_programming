97917116a6fa63aae653587cb7e843e073ea9009
julien3641 <3641@holbertonschool.com>
2021-11-03 14:42:20 +0100

    first files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - additions
+ * @a: a
+ * @b: b
+ * Return: Sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Difference
+ * @a: a
+ * @b: b
+ * Return: Difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplication
+ * @a: a
+ * @b: b
+ * Return: Multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Division
+ * @a: a
+ * @b: b
+ * Return: Division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: a
+ * @b: b
+ * Return: Modulo
+ */
+
+nt op_mod(int a, int b)
+{
+	return (a % b);
+}
