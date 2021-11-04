9c35c6fc8d470b630b187042e58678ae4d07a9a7
julien3641 <3641@holbertonschool.com>
2021-11-04 17:46:51 +0100

    print all arguments

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,90 @@
+#include "variadic_functions.h"
+
+/**
+ * print_char - print char
+ * @separator: Separator
+ * @args: list of variadic
+ */
+
+void print_char(char *separator, va_list args)
+{
+	printf("%s%c", separator, va_arg(args, int));
+}
+
+/**
+ * print_integer - print integer
+ * @n: n
+ */
+
+void print_integer(char *separator, va_list args)
+{
+	printf("%s%i", separator, va_arg(args, int));
+}
+
+/**
+ * print_float - float
+ * @separator: separator
+ * @args: args
+ */
+
+void print_float(char *separator, va_list args)
+{
+	printf("%s%f", separator, va_arg(args, double));
+}
+
+/**
+ * print_string - print string
+ * @separator: separator
+ * @args: args
+ */
+
+void print_string(char *separator, va_list args)
+{
+	char *arg = va_arg(args, char *);
+
+	if (arg == NULL)
+	{
+		printf("%s%s", separator, "(nil)");
+		return;
+	}
+	printf("%s%s", separator, arg);
+}
+
+
+/**
+ * print_all - print all arguments
+ * @format: format
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	unsigned int i = 0, j = 0;
+	type_t remi[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	char *separator = "";
+	va_start(ap, format);
+	i = 0;
+	while (format != NULL && format[i])
+	{
+		j = 0;
+		while (remi[j].type)
+		{
+			if (format[i] == *remi[j].type)
+			{
+				remi[j].f(separator, ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,4 +5,20 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(char *separator, va_list args);
+void print_integer(char *separator, va_list args);
+void print_float(char *separator, va_list args);
+void print_string(char *separator, va_list args);
+/**
+ * struct type - struc type
+ * @f: f
+ * @type: type
+ */
+
+typedef struct type
+{
+	char *type;
+	void (*f)(char *separator, va_list args);
+} type_t;
 #endif
