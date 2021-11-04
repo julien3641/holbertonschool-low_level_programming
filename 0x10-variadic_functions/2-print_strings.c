6039416c993f664cf7824aeaf9695fd9ee63159a
julien3641 <3641@holbertonschool.com>
2021-11-04 13:17:20 +0100

    betty issues

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,11 +15,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(arg, n);
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(arg, char*);
 		if (str == 0)
-			printf ("(nil)");
+			printf("(nil)");
 		else
 			printf("%s", str);
 		if (i != (n - 1) && separator != 0)
