ce7f009920306409536de0ce177460748bfbd3a4
julien3641 <3641@holbertonschool.com>
2021-11-12 16:00:37 +0100

    add text befor main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * start_before_main - start before main
+ */
+
+void start_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
