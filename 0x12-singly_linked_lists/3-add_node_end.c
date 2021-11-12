fb6608cd607159f676e58864f98f0f499cf6599c
julien3641 <3641@holbertonschool.com>
2021-11-12 14:41:17 +0100

    betty issues

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -47,7 +47,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		last = *head;
 		while (last->next != NULL)
-	       		last = last->next;
+			last = last->next;
 		last->next = new;
 		return (last);
 	}
