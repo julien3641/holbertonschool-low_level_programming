990ee15dd47ab0a02eef7d61e563729900924c58
julien3641 <3641@holbertonschool.com>
2021-11-15 11:30:28 +0100

    task 0 + lists.h

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list
+ * @h: h
+ * Return: 0
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	if (h)
+	{
+		for (i = 0; h; i++)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+	}
+	return (i);
+}
