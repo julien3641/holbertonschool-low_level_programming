99b3880ae1e729f300b2badd7c575d70370a4fb6
julien3641 <3641@holbertonschool.com>
2021-11-16 16:17:28 +0100

    delete a node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	}
 
-	for (i = 0; i < index -1 && tmp != NULL; i++)
+	for (i = 0; i < index - 1 && tmp != NULL; i++)
 	{
 		tmp = tmp->next;
 		if (tmp->next != NULL || tmp == NULL)
