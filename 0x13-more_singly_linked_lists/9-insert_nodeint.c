14ff3ca7baf7c49766c7d75bf6cfbe3fa1e76ac0
julien3641 <3641@holbertonschool.com>
2021-11-16 14:13:15 +0100

    change first if

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,9 +13,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *scan;
 	unsigned int i;
 
-	if (head == NULL || *head == NULL)
+	if (!head)
+	{
 		return (0);
-
+	}
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
