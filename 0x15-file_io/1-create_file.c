71137fc3c9828389581b9b94277b75fbd6c03a61
julien3641 <3641@holbertonschool.com>
2021-11-22 14:36:57 +0100

    remove malloc

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,15 +26,10 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	char *buf;
 
 	if (filename == NULL)
 		return (-1);
-	buf = malloc((_strlen(text_content)) * sizeof(char));
-	if (buf == NULL)
-	{
-		return (-1);
-	}
+
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
@@ -42,7 +37,6 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 		write(fd, text_content, _strlen(text_content));
 
-	free(buf);
 	close(fd);
 	return (1);
 }
