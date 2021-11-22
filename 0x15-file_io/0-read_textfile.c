8c2c0f398684e9abfa6a8bb941168e1ef521b555
julien3641 <3641@holbertonschool.com>
2021-11-22 13:25:40 +0100

    read and write a text in a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads an prints text into POSIX standard output
+ * @filename: filename
+ * @letters: letters
+ * Return: return 0 if fail and the number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t sz, szw;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		return (0);
+	}
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	sz = read(fd, buf, letters);
+	close(fd);
+	if (sz < 1)
+	{
+		free(buf);
+		return (0);
+	}
+	szw = write(1, buf, sz);
+	free(buf);
+	if (sz != szw)
+		return (0);
+	return (szw);
+}
