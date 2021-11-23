cb3b3152b6a6205e08f063e0b7db2ac8ca9f18d9
julien3641 <3641@holbertonschool.com>
2021-11-23 14:38:44 +0100

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef DEF_0X2
+#define DEF_0X2
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,3 +12,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+#endif
