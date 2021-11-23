fb6f4c2f0e8775a9c280f377bc7cce5e9b500f30
julien3641 <3641@holbertonschool.com>
2021-11-23 15:37:38 +0100

    print the 50 number of the fibonacci number

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - 50 first number of Fibonnacci
+ * Return - return 0
+ */
+
+int main(void)
+{
+	long int f1 = 0, f2 = 1, i;
+	long int next;
+
+	for (i = 0; i < 50; i++)
+	{
+		{
+			next = f1 + f2;
+			printf("%ld", next);
+			f1 = f2;
+			f2 = next;
+			if (i != 49)
+				printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
