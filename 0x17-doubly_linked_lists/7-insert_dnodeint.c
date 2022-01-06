4f0cb53098cdf3b3f1878465cd37f726204e9051
julien3641 <3641@holbertonschool.com>
2022-01-06 14:39:45 +0100

    betty issues

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	new_node  = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-                return (0);
+		return (0);
 	tmp = *h;
 	while (i < idx - 1)
 	{
