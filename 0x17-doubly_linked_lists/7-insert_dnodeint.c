94ed4db78f99ad2179e49c2af7e25f84665f368e
julien3641 <3641@holbertonschool.com>
2022-01-06 14:38:42 +0100

    insert a node at an index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 
+
 /**
  * insert_dnodeint_at_index - inserts a new node at an index
  * @h: pointer to the first node
@@ -10,27 +11,35 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *tmp = *h;
+	dlistint_t *new_node, *tmp;
 	unsigned int i = 0;
 
+	if (*h == NULL)
+	{
+		new_node = add_dnodeint_end(h, n);
+		return (new_node);
+	}
+	if (idx == 0)
+	{
+		new_node = add_dnodeint(h, n);
+		return (new_node);
+	}
 	new_node  = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	if (h == NULL && idx == 0)
-		return (0);
-	while (i < idx - 1 && tmp != NULL)
+                return (0);
+	tmp = *h;
+	while (i < idx - 1)
 	{
 		tmp = tmp->next;
+		if (tmp == NULL)
+			return (0);
 		i++;
 	}
 	new_node->n = n;
 	new_node->next = tmp->next;
 	new_node->prev = tmp;
-	if (tmp->next != NULL)
-		tmp->next->prev = new_node;
 	tmp->next = new_node;
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
 	return (new_node);
 }
