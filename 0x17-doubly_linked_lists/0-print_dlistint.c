6172bbeb4b0a16703e958ba6271615335d6490af
julien3641 <3641@holbertonschool.com>
2022-01-06 12:04:38 +0100

    print the number of list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i;
+	int i = 0;
 
 	if (h)
-		for (i = 0; h; i++)
+		while (h)
 		{
 			printf("%d\n", h->n);
 			h = h->next;
+			i++;
 		}
 	return (i);
 }
