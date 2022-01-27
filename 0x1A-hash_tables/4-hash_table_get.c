82193855d028057ca036626e16365815d896a014
julien3641 <3641@holbertonschool.com>
2022-01-27 17:02:47 +0100

    retrieve a value associeted with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: hash table
+ * @key: the key you are looking for
+ * Return: the value associated or NULL if key not found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int idx;
+
+	idx = key_index((unsigned char *)key, ht->size);
+	if (ht->array[idx] == NULL)
+		return (NULL);
+	else
+		return (ht->array[idx]->value);
+}
