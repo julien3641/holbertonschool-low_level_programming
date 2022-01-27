afd31dd0a1bd5a5505041d2f59fcf203475e2f4e
julien3641 <3641@holbertonschool.com>
2022-01-27 19:56:42 +0100

    betty style

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: the hash table
+ * Return: nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	int check_first = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (i <= ht->size)
+	{
+		while (ht->array[i] != NULL)
+		{
+			if (check_first == 0)
+			{
+				printf("'%s': '%s'",
+				       ht->array[i]->key, ht->array[i]->value);
+				check_first = 1;
+			}
+			else if (check_first == 1)
+				printf(", '%s': '%s'",
+				       ht->array[i]->key, ht->array[i]->value);
+			ht->array[i] = ht->array[i]->next;
+		}
+		i++;
+	}
+	printf("}");
+	printf("\n");
+}
