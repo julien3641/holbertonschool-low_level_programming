d80aeaa178ce6c1c342511001458682c85398c98
julien3641 <3641@holbertonschool.com>
2022-03-17 13:34:49 +0100

    update code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,7 +9,7 @@ def island_perimeter(grid):
     """
     count = 0
     for i in range(1, len(grid) - 1):
-        for j in range(1, (len(grid[i]))):
+        for j in range(1, len(grid[i])):
             if grid[i][j] == 1:
                 if grid[i-1][j] == 0:
                     count += 1
