05fc150f8e3fe7c71dad10e89a507893ab4189f2
julien3641 <3641@holbertonschool.com>
2022-03-17 14:50:15 +0100

    task 6

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,8 +11,8 @@ def island_perimeter(grid):
     :return:
     """
     count = 0
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i])):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if grid[i-1][j] == 0:
                     count += 1
